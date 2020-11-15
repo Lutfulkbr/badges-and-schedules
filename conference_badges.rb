97920a3d50b10cd38a16978fcfbec631406d16ef
github username <github email address>
2020-11-15 13:47:34 -0500

    commit tree d40ad153ef6097578078e702dc1a93fc061347ed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    new_names = []
+    names.each { |name| new_names << "Hello, my name is #{name}."}
+    new_names
+end
+
+def assign_rooms(names)
+    speaker_room = []
+    names.each { |speaker| speaker_room << "Hello, #{speaker}! You'll be assigned to room #{speaker_room.length + 1}!"}
+    speaker_room
+end
+
+def printer(names)
+    batch_badge_creator(names).each { |name| puts name}
+    assign_rooms(names).each { |name| puts name}
+end
